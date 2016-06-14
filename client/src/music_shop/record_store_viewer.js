33a8de70239dac015f420be8233d776782800818
Aidan Pinkman <user@CODECLAN19.local>
2016-06-14 09:32:26 +0100

    new repo

--- client/src/music_shop/record_store_viewer.js
+++ client/src/music_shop/record_store_viewer.js
@@ -6,7 +6,7 @@ var Customer = require('./customer.js');
 var Viewer = function(musicShop){
 
     this.shop = musicShop;
-    console.log(this.shop);
+    // console.log(this.shop);
 }
 
 Viewer.prototype = {
@@ -19,16 +19,22 @@ Viewer.prototype = {
   info.innerText = "Welcome to " + this.shop.name + ", Proudly serving " + this.shop.city + " for all your music needs."
 
   this.shop.inventory();
- var addStock = document.getElementById('add-stock')
- addStock.onclick = function(event){
-  var newArtist = document.getElementById('artist')
-  var newTitle = document.getElementById('title')
-  var newPrice = document.getElementById('price')
+
+  },
+
+  newStock: function(){
+    var addStock = document.getElementById('add-stock')
+    var newArtist = document.getElementById('#artist');
+    var newTitle = document.getElementById('#title');
+    var newPrice = document.getElementById('#price');
+    addStock.onclick = function(){
+    
   
-  this.shop.addRecord(newArtist.value, newTitle.value, newPrice.value)
-  this.shop.inventory;
-    }.bind(this);
-  }
+      this.shop.addRecord(newArtist.value, newTitle.value, newPrice.value)
+      this.shop.inventory();
+      this.render();
+      }.bind(this); console.log(this.shop.stock);
+    }
 }
 
 module.exports = Viewer;
